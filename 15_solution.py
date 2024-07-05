edf865ee0ad529b6f3815e6a7b655e371d122db3
kwang1215 <xman1215@naver.com>
2024-07-05 11:34:03 +0900

    나머지가1이되는수찾기

---
+++ 15_solution.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 1
+    while True:
+        if n % answer == 1:
+            return print(answer)
+        else:
+            answer += 1
+
+
+solution(10)
+solution(12)
