d69970adf518e876427420c633c5aa04613fcea3
kwang1215 <xman1215@naver.com>
2024-07-11 17:06:43 +0900

    30_가운데 글자 가져오기

---
+++ 30_solution.py
@@ -0,0 +1,14 @@
+def solution(words):
+    if len(words) % 2 == 1:
+        answer = words[(len(words) // 2)]
+    else:
+        answer = words[(len(words) // 2 - 1): (len(words) // 2 + 1)]
+    return answer
+
+
+print(solution('abcde'))
+print(solution('qwer'))
+
+
+
+
