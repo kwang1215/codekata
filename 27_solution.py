0f3e1483c7b7a9ca8a2d659bd2cd2b51a5d43379
kwang1215 <xman1215@naver.com>
2024-07-11 17:19:17 +0900

    27_핸드폰 번호 가리기

--- 27_solution.py
+++ 27_solution.py
@@ -1,13 +1,18 @@
-def solution(phone_number):
-    replace_num = list(phone_number[:-4])
-    result = []
-    for i in range(len(replace_num)):
-        result.append('*')
+# 핸드폰 번호 가리기
 
-    result = ''.join(result)
-    answer = result + phone_number[-4:]
+# def solution(phone_number):
+#     replace_num = list(phone_number[:-4])
+#     result = []
+#     for i in range(len(replace_num)):
+#         result.append('*')
+#
+#     result = ''.join(result)
+#     answer = result + phone_number[-4:]
+#     return answer
+
+def solution(numbers):
+    answer = '*' * len(numbers[:-4]) + numbers[-4:]
     return answer
 
 
-print(solution('0123456789'))
-
+print(solution('01042069433'))
