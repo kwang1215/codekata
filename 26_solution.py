73b6f4345aabee965ec7a6d9a1001696cf06c4d2
kwang1215 <xman1215@naver.com>
2024-07-11 09:50:06 +0900

    26_음양 더하기

---
+++ 26_solution.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    result = []
+    for i, sign in enumerate(signs):
+        if sign == True:
+            result.append(absolutes[i])
+        else:
+            result.append(-(absolutes[i]))
+    answer = sum(result)
+    return answer
